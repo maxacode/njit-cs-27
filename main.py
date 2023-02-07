ebdf6de70f89190874c648f4ab8007daf4d030a7
Maks Derevencha <maxderevencha@gmail.com>
2023-02-06 21:28:26 -0500

    update

--- main.py
+++ main.py
@@ -50,26 +50,74 @@
 # For Loop example:
 
 
-names = ['maks derevencha', 'mike folk', 'jayden rodriquez']
+# names = ['maks beiber', 'britany spears', 'barbie doll']
 
-for tempVariable in names:
-    #email = first + . + last + @company.com
-    # maks.derevencha@company.com
-    splitNames = tempVariable.split(" ") # ['maks', 'derevencha']
-    email = splitNames[0] + '.' + splitNames[1] + "@company.com"
-    print(email)
-
-
+# for tempVariable in names:
+#     #email = first + . + last + @company.com
+#     # maks.derevencha@company.com
+#     splitNames = tempVariable.split(" ") # ['maks', 'derevencha']
+#     email = splitNames[0] + '.' + splitNames[1] + "@company.com"
+#     print(email)
 
 
 
 
 
+# names = ['maks beiber', 'britany spears', 'barbie doll', 'joe dirt', 'mcgyver', 'james bond']
+
+# #   start, end, stepby/counter/incriment 
+# # range( 0, 10, 2)
+
+# # empty list named peopleInvited
+# peopleInvited = []
+# for x in range(0, len(names),2):
+#     peopleInvited.append(names[x])
+#     print(peopleInvited)
+
+# while len(peopleInvited) <= 3:
+#     print(f'Here is an invitation to my party: {peopleInvited[0]}')
+     
+
+
+
+# names = ['maks beiber', 'britany spears', 'barbie doll', 'joe dirt', 'mcgyver', 'james bond']
+
+
+# maxPeople = 5
+
+# for x in names:
+#     while maxPeople > 0:
+#         maxPeople -= 1
+
+#         print(f"You are invited to my party: {x}")
 
 
 
 
+# Mixing conditions
+
+# names = ['maks beiber', 'britany spears', 'barbie doll', 'joe dirt', 'mcgyver', 'james bond']
+
+#   start, end, stepby/counter/incriment 
+# range( 0, 10, 2)
+
+# # empty list named peopleInvited
+# peopleInvited = []
+# for x in range(0, len(names),2):
+#     if "maks" in names[x]:
+#         print(f"YOu are not invited {names[x]}")
+#         continue
+        
+#     print(f"Your invited to my party: {(names[x])}")
+
+ 
+
+# infinite loop/controlled
 
 
-
-
+x = 1
+#x = 0, 1, 2,3,4,5
+while x > 0:
+    x += 1 
+    print(x)
+    input('press enter to continue')
